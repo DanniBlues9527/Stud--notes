8babafaecfe5ab1e67769b2ebf877cbb8b910f5c
danniblues <danniblues9527@gmail.com>
2021-10-11 13:50:24 +0800

    add react-redux_开发者工具

---
+++ react_extension/src/components/1_setState/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class Demo extends Component {
+  state = { count: 0 };
+
+  add = () => {
+    //对象式的setState
+    // const {count} = this.state
+    // this.setState({count:count+1},() => {
+    //   console.log(this.state.count)
+    // })
+
+    //函数式的setState
+    this.setState((state, props) => {
+      return { count: state.count + 1 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>当前求和状态为：{this.state.count}</h1>
+        <button onClick={this.add}>点我+1</button>
+      </div>
+    );
+  }
+}
